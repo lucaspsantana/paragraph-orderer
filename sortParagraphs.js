62b5be965abe2466321f20bfef4d1128cd453446
Lucas Santana <lucas.pesanttana@gmail.com>
2022-01-07 16:40:22 -0300

    first commit

---
+++ sortParagraphs.js
@@ -0,0 +1,10 @@
+function sortParagraphs(text) {
+    let formatedText;
+    let separateParagraphs = text.split("\n");
+    let ordenateParagraphs = separateParagraphs.sort();
+    ordenateParagraphs.forEach(element => {
+        formatedText = `${formatedText}\n${element}\n`;
+    });
+
+    return formatedText;
+}
