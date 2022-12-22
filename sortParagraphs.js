a91fe7df62c4b8e5fce898f631bafa8f6fa11dcd
Lucas Santana <lucas.pesanttana@gmail.com>
2022-12-21 22:59:39 -0300

    fix sort logic

--- sortParagraphs.js
+++ sortParagraphs.js
@@ -1,7 +1,7 @@
 function sortParagraphs(text) {
     let formatedText="";
     let separateParagraphs = text.split("\n");
-    let ordenateParagraphs = separateParagraphs.sort();
+    let ordenateParagraphs = separateParagraphs.sort((a, b) => a.localeCompare(b));
     ordenateParagraphs.forEach(element => {
         formatedText = `${formatedText}\n${element}`;
     });
